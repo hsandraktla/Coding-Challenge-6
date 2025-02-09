c75dcf6f2279c47eb24bd5b1e388f0fff426df93
H Sandra Ktla <hsandraktla@usf.edu>
2025-02-09 15:39:06 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,14 @@ function calculateProfit(costPrice, sellingPrice, untisSold) {
 //Log the results.
 console.log(`Total Profit: $${calculateProfit(20, 30, 100)}`); //Expected output: "Total Profit: $1000"
 console.log(`Total Profit: $${calculateProfit(50, 70, 200)}`); //Expected output: "Total Profit: $4000"
+
+
+//Task 2: Sales Tax Computation - Function Expression
+//Declare a function expression calculateSalesTax(amount, taxRate) that calculates sales tax.
+let calculateSalesTax = function(amount, taxRate) {
+    let salesTax = amount * taxRate; //The formula for calculating sales tax.
+    return Math.round(salesTax);
+};
+//Log the results.
+console.log(`Sales Tax: $${calculateSalesTax(100, 0.07)}`); //Expected output: "Sales Tax: $7"
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`); //Expected output: "Sales Tax: $50"
